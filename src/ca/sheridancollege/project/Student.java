e99f1356598650f5d72a2638f1b8399503b6b89c
rjrnaik <naikra@sheridancollege.ca>
2022-06-08 22:36:11 -0400

    First Commit_V1.0

---
+++ src/ca/sheridancollege/project/Student.java
@@ -0,0 +1,32 @@
+package ca.sheridancollege.project;
+
+public class Student {
+
+	private int age;
+	private int size;
+
+	public int getAge() {
+		return this.age;
+	}
+
+	/**
+	 * 
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getSize() {
+		return this.size;
+	}
+
+	/**
+	 * 
+	 * @param size
+	 */
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
